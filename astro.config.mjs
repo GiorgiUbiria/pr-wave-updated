c08bd2ef9f454f60b64b0f7bd8a03dd4193c087c
Giorgi Ubiria <ubiriagiorgi8@gmail.com>
2024-10-01 15:58:08 +0400

    add: initial commmit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import db from '@astrojs/db';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), alpinejs(), db()],
+  vite: {
+    ssr: {
+      noExternal: ['three'],
+    },
+  },
+});
---
+++ db/config.ts
@@ -0,0 +1,16 @@
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Comment = defineTable({
+  columns: {
+    authorId: column.number(),
+    published: column.date(),
+    body: column.text(),
+  },
+  indexes: [
+    { on: ["authorId", "published"], unique: true },
+  ]
+});
+
+export default defineDb({
+  tables: { Comment }
+});
